da114bf9bcc8a278d439b49adb814106556fdb62
Mikhail-wd <Supalis913@yahoo.com>
2024-08-20 16:33:43 +0700

    fixes added

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,7 +5,7 @@ import Button from "@mui/material/Button"
 import { CircularProgress } from "@mui/material"
 import { useState, useContext } from "react"
 import { useNavigate } from "react-router-dom"
-import {ContextLogin} from "../App.js"
+import { ContextLogin } from "../App.js"
 
 function LoginPage() {
     const contextLogin = useContext(ContextLogin)
@@ -14,6 +14,8 @@ function LoginPage() {
         mail: "",
         password: ""
     })
+    const [errorMailText, setErrorMailText] = useState("* Введие правельную электронную почту")
+    const [errorPasswordText, setErrorPasswordText] = useState("* Пароль не содержит нужных символов или отсутствует")
     const [errorMail, setErrorMail] = useState(false)
     const [errorPassword, setErrorPassword] = useState(false)
     const [authProcess, setAuth] = useState(false)
@@ -37,6 +39,7 @@ function LoginPage() {
 
     function authCheck() {
         if (state.mail.match(/^\S+@\S+\.\S+$/) === null) {
+            setErrorMailText("* Введие правельную электронную почту")
             setErrorMail(true)
         }
         if (state.mail.match(/^\S+@\S+\.\S+$/) !== null) {
@@ -44,10 +47,11 @@ function LoginPage() {
             if (state.password.match(/[A-Z]/g) !== null && state.password.toString().length >= 8) {
                 setAuth(true)
                 fakeFetch()
-                contextLogin.dispatch({type:"login"})
+                contextLogin.dispatch({ type: "login" })
             }
         }
-        if (state.password.match(/[A-Z]/g) === null || state.password.toString().length <= 7) {
+        if (state.password.match(/[A-Z]/g) === null) {
+            setErrorPasswordText("* Пароль не содержит нужных символов или отсутствует")
             setErrorPassword(true)
         }
         if (state.password.match(/[A-Z]/g) !== null && state.password.toString().length >= 8) {
@@ -55,7 +59,7 @@ function LoginPage() {
             if (state.mail.match(/^\S+@\S+\.\S+$/) !== null) {
                 setAuth(true)
                 fakeFetch()
-                contextLogin.dispatch({type:"login"})
+                contextLogin.dispatch({ type: "login" })
             }
         }
     }
@@ -65,13 +69,12 @@ function LoginPage() {
             <Box sx={{ width: "80%", display: "flex", gap: "30px 10%", flexWrap: "wrap", backgroundColor: "#474764", margin: "0px auto", minHeight: "81dvh", justifyContent: "center", paddingTop: "8%" }}>
                 <Box component="form" autoComplete="off" sx={{ alignItems: "center", width: "600px", display: "flex", flexDirection: "column", gap: "30px 10%", flexWrap: "wrap" }} >
                     {!errorMail ?
-
                         <TextField disabled={authProcess ? true : null} hiddenLabel label="Электронная почта" id="outlined" variant="outlined" sx={{ width: "100%" }} value={state.mail} onChange={(event) => handlerMail(event)} /> :
-                        <TextField hiddenLabel error label="Электронная почта" id="outlined-basic" variant="outlined" sx={{ width: "100%" }} value={state.mail} onChange={(event) => handlerMail(event)} />
+                        <TextField hiddenLabel error label="Электронная почта" helperText={errorMailText} id="outlined-basic" variant="outlined" sx={{ width: "100%" }} value={state.mail} onChange={(event) => handlerMail(event)} />
                     }
                     {!errorPassword ?
                         <TextField disabled={authProcess ? true : null} hiddenLabel type="password" id="outlined" label="Пароль" variant="outlined" sx={{ width: "100%" }} value={state.password} onChange={(event) => handlerPassword(event)} /> :
-                        <TextField hiddenLabel type="password" error id="outlined-basic" label="Пароль" variant="outlined" sx={{ width: "100%" }} value={state.password} onChange={(event) => handlerPassword(event)} />}
+                        <TextField hiddenLabel type="password" error id="outlined-basic" helperText={errorPasswordText} label="Пароль" variant="outlined" sx={{ width: "100%" }} value={state.password} onChange={(event) => handlerPassword(event)} />}
 
                     <Button disabled={authProcess ? true : null} variant="contained" sx={{ width: "fit-content", padding: "10px 24px", fontSize: "22px", fontWeight: 700, color: "white", backgroundColor: "#474764" }} onClick={() => authCheck()}>{authProcess ?
                         <Box sx={{ display: 'flex', margin: "0px 20px 0px 0px" }}>
